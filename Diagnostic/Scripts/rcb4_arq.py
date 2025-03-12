81f8d975aa8946a4701f0443bc14e39abcffdfc9
spanovs01 <spanovs01frag@egmail.com>
2025-03-12 12:34:39 +0300

    Remove outdated tests

--- Diagnostic/Scripts/rcb4_arq.py
+++
@@ -1,8 +0,0 @@
-import rokilowlvlpy as roki
-import rokipytest as rpt
-
-mb = rpt.create_motherboard()
-rcb = roki.protocols.Rcb4(mb)
-
-rpt.call(rcb, rcb.enable_arq(42))
-rpt.call(mb, mb.disable_arq())
